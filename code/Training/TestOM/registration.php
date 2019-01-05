833fcdbc5fcb70364b917d51c700e310eb30a77c
rustem.mageinn <rustem.mageinn@gmail.com>
2019-01-05 23:11:48 +0300

    fix

--- code/Training/TestOM/registration.php
+++ code/Training/TestOM/registration.php
@@ -1,8 +1,4 @@
 <?php
-/**
- * Copyright © 2015 Scommerce Mage. All rights reserved.
- * See COPYING.txt for license details.
- */
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
