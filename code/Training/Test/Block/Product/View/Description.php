95ab1d5f2c0e0aa0dfa56faafc9df59c7a2a6ffd
rustem.mageinn <rustem.mageinn@gmail.com>
2019-01-05 23:02:02 +0300

    Ex. 3.7

--- code/Training/Test/Block/Product/View/Description.php
+++ code/Training/Test/Block/Product/View/Description.php
@@ -8,6 +8,6 @@ class Description
     public function beforeToHtml(
         \Magento\Catalog\Block\Product\View\Description $subject
     ) {
-        return $subject->getProduct()->setDescription('Test Description');
+        $subject->setTemplate('Training_Test::description.phtml');
     }
 }
